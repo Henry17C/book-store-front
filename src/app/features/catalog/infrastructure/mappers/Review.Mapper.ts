d736af552dbe130dc10f5f8ee4a2a84586b4e5ed
Henry17C <henryisrael45@gmail.com>
2026-08-24 18:12:16 -0500

    feat(catalog): add toPaginated method

--- src/app/features/catalog/infrastructure/mappers/Review.Mapper.ts
+++ src/app/features/catalog/infrastructure/mappers/Review.Mapper.ts
@@ -1,4 +1,6 @@
+import { Paginated } from '../../domain/models/paginated.model';
 import { Review, UpdateReviewPayload, WriteReviewPayload } from '../../domain/models/review.model';
+import { PageResponseDTO } from '../dtos/book.dto';
 import {
   ReviewDetailsResponseDTO,
   UpdateReviewRequestDTO,
@@ -6,33 +8,45 @@ import {
 } from '../dtos/review.dto';
 
 export class ReviewMapper {
-    //to Domain
-    static toDomain(dto: ReviewDetailsResponseDTO): Review {
-        return {
-        id: dto.id,
-        bookId: dto.bookId,
-        userId: dto.userId,
-        rating: dto.rating,
-        comment: dto.comment,
-        };
-    }
+  //to Domain
+  static toDomain(dto: ReviewDetailsResponseDTO): Review {
+    return {
+      id: dto.id,
+      bookId: dto.bookId,
+      userId: dto.userId,
+      rating: dto.rating,
+      comment: dto.comment,
+    };
+  }
 
-    //toDTO
-    static toUpdateReviewRequestDTO(payload: UpdateReviewPayload): UpdateReviewRequestDTO {
-        return {
-        bookId: payload.bookId,
-        userId: payload.userId,
-        rating: payload.rating,
-        comment: payload.comment,
-        };
-    }
+  //toDTO
+  static toUpdateReviewRequestDTO(payload: UpdateReviewPayload): UpdateReviewRequestDTO {
+    return {
+      bookId: payload.bookId,
+      userId: payload.userId,
+      rating: payload.rating,
+      comment: payload.comment,
+    };
+  }
 
-    static toWriteReviewRequestDTO(payload: WriteReviewPayload): WriteReviewRequestDTO {
-        return {
-        bookId: payload.bookId,
-        userId: payload.userId,
-        rating: payload.rating,
-        comment: payload.comment,
-        };
-    }
+  static toWriteReviewRequestDTO(payload: WriteReviewPayload): WriteReviewRequestDTO {
+    return {
+      bookId: payload.bookId,
+      userId: payload.userId,
+      rating: payload.rating,
+      comment: payload.comment,
+    };
+  }
+
+  static toPaginated<DTO, Domain>(
+    dto: PageResponseDTO<DTO>,
+    mapItemFn: (item: DTO) => Domain,
+  ): Paginated<Domain> {
+    return {
+      items: dto.content.map(mapItemFn),
+      currentPage: dto.currentPage,
+      totalPages: dto.totalPages,
+      totalElements: dto.totalElements,
+    };
+  }
 }
