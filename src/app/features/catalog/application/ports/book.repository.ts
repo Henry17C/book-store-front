1250b0d101133ff9497246bc0866f5523ddbef0b
Henry17C <henryisrael45@gmail.com>
2026-08-24 17:48:21 -0500

    feat(catalog): change return type to CatalogBook

--- src/app/features/catalog/application/ports/book.repository.ts
+++ src/app/features/catalog/application/ports/book.repository.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import {
     Book,
+    CatalogBook,
     RegisterBookPayload,
     UpdateBookPayload,
 } from '../../domain/models/book.model';
@@ -8,7 +9,7 @@ import { Paginated } from '../../domain/models/paginated.model';
 
 export abstract class BookRepository {
     abstract getBookDetails(id: string): Observable<Book>;
-    abstract getCatalogPage(page: number, size: number): Observable<Paginated<Book>>;
+    abstract getCatalogPage(page: number, size: number): Observable<Paginated<CatalogBook>>;
 
     abstract registerBook(payload: RegisterBookPayload): Observable<void>;
     abstract updateBook(id: string, payload: UpdateBookPayload): Observable<void>;
@@ -16,7 +17,7 @@ export abstract class BookRepository {
     abstract removeBookRecommendation(id: string): Observable<void>;
     abstract searchBooks(keyword: string, page: number, size: number,
         onlyInStock: boolean,
-    ): Observable<Paginated<Book>>;
+    ): Observable<Paginated<CatalogBook>>;
 
     abstract unarchiveBook(id: string): Observable<void>;
     abstract archiveBook(id: string): Observable<void>;
