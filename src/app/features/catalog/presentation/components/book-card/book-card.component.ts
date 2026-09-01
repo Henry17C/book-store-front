f7a679964c977bf71f74292b435bfe6cc9ad436b
Henry17C <henryisrael45@gmail.com>
2026-09-01 10:56:21 -0500

    feat(catalog): bind real API data to BookCard template

--- src/app/features/catalog/presentation/components/book-card/book-card.component.ts
+++ src/app/features/catalog/presentation/components/book-card/book-card.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Book, CatalogBook } from '../../../domain/models/book.model';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
 @Component({
   selector: 'app-book-card',
-  imports: [],
+  imports: [CurrencyPipe,DecimalPipe],
   templateUrl: './book-card.component.html',
   styles: ``,
 })
 export class BookCard {
-  
+  @Input({ required: true }) book!: CatalogBook;
 }
