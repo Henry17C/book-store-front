3d14a1356b39097591c32b1ef0541ca062bea3e9
Henry17C <henryisrael45@gmail.com>
2026-09-01 10:42:59 -0500

    feat(catalog): configure HTTP client and BookRepository provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { BookRepository } from './features/catalog/application/ports/book.repository';
+import { BookHttpService } from './features/catalog/infrastructure/adapters/book-http.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    // Enlazar el puerto (abstracción) con el adaptador (implementación)
+    { provide: BookRepository, useClass: BookHttpService }
   ]
 };
