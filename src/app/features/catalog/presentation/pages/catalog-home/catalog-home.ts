610f089fa96acb9ec36c6b9ecbee7ba0ad302812
Henry17C <henryisrael45@gmail.com>
2026-09-01 10:46:21 -0500

    feat(catalog): connect CatalogHome to real API via CatalogFacade

--- src/app/features/catalog/presentation/pages/catalog-home/catalog-home.ts
+++ src/app/features/catalog/presentation/pages/catalog-home/catalog-home.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BookCarouselComponent } from '../../components/book-carousel/book-carousel.component.';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { CatalogFacade } from '../../../application/facade/catalog.facade';
+import { CatalogBook } from '../../../domain/models/book.model';
 @Component({
   selector: 'app-catalog-home',
-  imports: [BookCarouselComponent],
+  standalone: true,
+  imports: [CommonModule, BookCarouselComponent, AsyncPipe],
   templateUrl: './catalog-home.html',
-
 })
-export class CatalogHome {}
+export class CatalogHome {
+  private readonly catalogFacade = inject(CatalogFacade);
+  newReleases$: Observable<CatalogBook[]> = this.catalogFacade.getNewReleases();
+  bestSellers$: Observable<CatalogBook[]> = this.catalogFacade.getBestSellers();
+  recommended$: Observable<CatalogBook[]> = this.catalogFacade.getRecommended();
+
+}
