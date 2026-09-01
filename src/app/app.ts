5d367a0a133c2f26a9c4cf4be623362ba253c31b
Henry17C <henryisrael45@gmail.com>
2026-09-01 11:06:04 -0500

    feat(app): assemble main layout with header, catalog home, and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CatalogHome } from './features/catalog/presentation/pages/catalog-home/catalog-home';
+import { BottomNav } from './features/catalog/presentation/layout/bottom-nav/bottom-nav.component';
+import { Header } from './features/catalog/presentation/layout/header/header.component';
+import { BookDetails } from './features/catalog/presentation/pages/book-details/book-details.component';
+import { Footer } from './features/catalog/presentation/layout/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [BookDetails,RouterOutlet,CatalogHome, BottomNav,Header,Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('book-store-front');
